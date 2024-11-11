c18a29b2aefe70f5a3171d1a8b0481a4cf1934ce
Luis Rosas <luis.angel.833@hotmail.com>
2024-11-11 00:07:33 -0600

    Initial commit

---
+++ app/src/main/java/rosas/luis/mymedicine/AddMedicine.kt
@@ -0,0 +1,13 @@
+/*
+    En esta clase se hará el registro de medicamentos donde se incluirá:
+    - Nombre del medicamento
+    - Indicaciones
+    - Indicaciones adicionales
+    - Fecha de vencimiento
+    - Cantidad (gr, ml, ...)
+ */
+
+package rosas.luis.mymedicine
+
+class AddMedicine {
+}
